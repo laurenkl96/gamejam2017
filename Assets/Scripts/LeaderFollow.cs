e5809e2693992deeff4b9392ea46db516ed57fca
Lauren Lynch <Lauren Lynch>
2017-02-11 12:07:06 -0600

    test files

---
+++ Assets/Scripts/LeaderFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LeaderFollow : MonoBehaviour
+{
+    public float moveSpeed;
+    public Transform lead;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = lead.position - this.transform.position;
+        direction.y = 0;
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+        this.transform.Translate(0, 0, moveSpeed);
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+    public GameObject player;
+    Vector3 offset;
+
+    //Start is used at initiallization
+    void Start()
+    {
+        offset = player.transform.position - transform.position;
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        float angle = player.transform.eulerAngles.y;
+        Quaternion rot = Quaternion.Euler(0, angle, 0);
+        transform.position = player.transform.position - (rot * offset);
+        transform.LookAt(player.transform);
+	}
+}
